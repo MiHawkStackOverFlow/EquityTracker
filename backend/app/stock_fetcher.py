b008e04c128f6ea816cac6cd733041099e392149
Abhishek Sharma <abhivsharma8@gmail.com>
2025-05-18 18:36:33 +0530

    [Backend] Trigger redeploy with Finnhub env injection

--- backend/app/stock_fetcher.py
+++ backend/app/stock_fetcher.py
@@ -1,18 +1,32 @@
-import yfinance as yf
+import os
+import requests
+from pathlib import Path
+from dotenv import load_dotenv
+
+env_path = Path(__file__).resolve().parent.parent / ".env"
+load_dotenv(dotenv_path=env_path)
+
+FINNHUB_API_KEY = os.getenv("FINNHUB_API_KEY")
 
 def fetch_stock_data(ticker: str) -> dict:
     try:
-        stock = yf.Ticker(ticker)
-        print("DEBUG1:", stock)
-        data = stock.info
-        print("DEBUG2:", data)
+        base_url = "https://finnhub.io/api/v1/quote"
+        params = { "symbol": ticker.upper(), "token": FINNHUB_API_KEY }
+
+        response = requests.get(base_url, params=params)
+        data = response.json()
+
+        if "c" not in data or data["c"] == 0:
+            return {"error": "No data found. Invalid ticker?"}
+
         return {
             "ticker": ticker.upper(),
-            "name": data.get("shortName"),
-            "price": data.get("regularMarketPrice"),
-            "currency": data.get("currency"),
-            "change_percent": data.get("regularMarketChangePercent"),
-            "exchange": data.get("exchange"),
+            "price": data["c"],
+            "high": data["h"],
+            "low": data["l"],
+            "open": data["o"],
+            "prev_close": data["pc"]
         }
+
     except Exception as e:
         return {"error": str(e)}
